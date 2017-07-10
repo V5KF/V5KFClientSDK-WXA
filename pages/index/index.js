de2d5362931fb8d624b51ec3d6dffee714897f3d
chyrain <chyrain_v5kf@qq.com>
2017-07-10 09:31:38 +0800

    去除无用功能，保留小程序客服按钮

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,20 +9,18 @@ Page({
   },
   startChat: function() {
     wx.navigateTo({
-      url: '../chat/chat?site=105723&oid=abcdefghijklmnopqrstuvwxyz&nickname='+this.data.userInfo.nickName+'&photo='+this.data.userInfo.avatarUrl+'&human=false&magic=[{"key":"会员等级","val":"VIP3"}]'
+      url: '../chat/chat?site=10000&nickname='+this.data.userInfo.nickName+'&photo='+this.data.userInfo.avatarUrl+'&human=false&magic=[{"key":"来源","val":"小程序"}]'
     });
   },
   onLoad: function () {
     console.log('onLoad');//////
-    // this.startChat();
-
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       console.log({'userinfo': userInfo});//////
       //更新数据
       that.setData({
-        motto: '25:00',
+        // motto: '25:00',
         userInfo:userInfo
       })
     });
@@ -39,21 +37,21 @@ Page({
       }
     });
 
-    var maxtime = 25*60; //1小时，按秒计算     
-    that.timer = setInterval(function(){
-      if(maxtime >= 0){   
-        var minutes = Math.floor(maxtime/60);   
-        var seconds = Math.floor(maxtime%60);   
-        that.setData({
-          motto: (minutes < 10 ? '0'+minutes : minutes)+':'+(seconds < 10 ? '0'+seconds : seconds)
-        });
-        --maxtime;
-      } else {   
-        clearInterval(that.timer);
-        that.setData({
-          motto: '预览时间结束'
-        });
-      }
-    }, 1000);
+    // var maxtime = 25*60; //1小时，按秒计算     
+    // that.timer = setInterval(function(){
+    //   if(maxtime >= 0){   
+    //     var minutes = Math.floor(maxtime/60);   
+    //     var seconds = Math.floor(maxtime%60);   
+    //     that.setData({
+    //       motto: (minutes < 10 ? '0'+minutes : minutes)+':'+(seconds < 10 ? '0'+seconds : seconds)
+    //     });
+    //     --maxtime;
+    //   } else {   
+    //     clearInterval(that.timer);
+    //     that.setData({
+    //       motto: '预览时间结束'
+    //     });
+    //   }
+    // }, 1000);
   } 
 })
